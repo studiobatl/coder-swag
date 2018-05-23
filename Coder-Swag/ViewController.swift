9b16ed5ace9bc84ed1a3e1cd85d7b87b272e6bb8
Brian O'Rourke <studiobatl@gmail.com>
2018-05-23 13:42:50 -0400

    Initial Commit

---
+++ Coder-Swag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coder-Swag
+//
+//  Created by Brian O'Rourke on 5/23/18.
+//  Copyright © 2018 Brian O'Rourke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
